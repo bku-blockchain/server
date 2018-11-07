05745f80163f2d55a16886946dfbf2929ac54f43
Huỳnh Hà <huynhsamha@gmail.com>
2018-11-07 08:06:31 +0700

    fix token expire

--- config/app.js
+++ config/app.js
@@ -3,10 +3,11 @@
  */
 
 const ONE_HOUR = 60 * 60; // in seconds
+const ONE_DAY = 24 * ONE_HOUR;
 
 module.exports = {
   secretKey: process.env.SECRET_KEY || 'server secret key',
-  tokenExpire: 2 * ONE_HOUR,
+  tokenExpire: 4 * ONE_DAY + 12 * ONE_HOUR,
 
   email: {
     address: process.env.EMAIL_ADDRESS || 'server email address',
