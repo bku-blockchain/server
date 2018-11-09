f98fe905e254a6ecf14a06f992a9a8c8a7b430e8
Huỳnh Hà <huynhsamha@gmail.com>
2018-11-09 23:52:08 +0700

    fix import script

--- scripts/import.sh
+++ scripts/import.sh
@@ -1,6 +1,6 @@
 # PWD at root of project
 # Run ./scripts/import.sh
-mongoimport --db _MoST --collection users --file ./db/export/users.json
-mongoimport --db _MoST --collection records --file ./db/export/records.json
-mongoimport --db _MoST --collection polls --file ./db/export/polls.json
-mongoimport --db _MoST --collection votes --file ./db/export/votes.json
+mongoimport --db MoST --collection users --file ./db/export/users.json
+mongoimport --db MoST --collection records --file ./db/export/records.json
+mongoimport --db MoST --collection polls --file ./db/export/polls.json
+mongoimport --db MoST --collection votes --file ./db/export/votes.json
